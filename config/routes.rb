bea83c35c800d78b903f4776c541f5b2e3894a77
dan@nerdi.com <dan@nerdi.com>
2013-12-03 17:41:39 +1100

    add very basic store view and store controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Ah::Application.routes.draw do
+  root 'store#index', as: 'store'
+  get "store/index"
   resources :products
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
