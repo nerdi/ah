b286ed963bbde7ab108c3d543a29fd709f09d41d
dan@nerdi.com <dan@nerdi.com>
2013-12-04 08:00:39 +1100

    add caching to store index

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
                                             with: %r{\.(gif|jpg|png)\Z}i,
                                             message: 'must be a URL or GIF, PNG, JPG or PNG image'}
   validates :title, length: {minimum: 15, maximum: 20}
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
