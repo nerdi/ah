809896ffd11be21d1aed8b77b6ad1054112b9cb9
dan@nerdi.com <dan@nerdi.com>
2013-12-05 07:50:15 +1100

    maybe this sorted out the belongs to issue with the migration

---
+++ db/migrate/20131203213249_create_line_items.rb
@@ -0,0 +1,9 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    create_table :line_items do |t|
+      t.references :product, index: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202222707) do
+ActiveRecord::Schema.define(version: 20131203214627) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "line_items", ["product_id"], name: "index_line_items_on_product_id"
 
   create_table "products", force: true do |t|
     t.string   "title"
